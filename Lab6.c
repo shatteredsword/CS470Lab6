47193266f6329b000e7aab4083f079d3056e2ec4
Sean Mayo <blackknight452@gmail.com>
2016-03-10 19:22:45 -0800

    finished lab

--- Lab6.c
+++ Lab6.c
@@ -5,31 +5,44 @@
 //#include <openssl/rand.h>
 //#include <time.h>
 
-#define N 710
+#define N 5
 //#define NUM_THREADS 16
 
 unsigned long input_matrix[N][N];
-	unsigned long long threadless[N][N], threaded[N][N], squared_threads[N][N];
+unsigned long long threadless[N][N], threaded[N][N], squared_threads[N][N];
 
 struct index
 {
-	int i;
-	int j;
+	int i; //row
+	int j; //column
 };
+
+struct row
+{
+	int j; //column
+};
+
+unsigned long get_random()
+{
+	return rand() % 100;
+}
 
 //The function used by threaded[N][N]
 static inline void *threaded_method(void *param)
 {
-	struct index *entry = param; // the structure that holds our data
-	int sum = 0; //the counter and sum
-
-	//Row multiplied by column
-	for(int n = 0; n < N; n++)
+	for (int i = 0; i < N; i++)
 	{
-		sum += input_matrix[entry -> i][n] * input_matrix[n][entry -> j];
+		struct row *entry = param; // the structure that holds our data
+		int sum = 0; //the counter and sum
+
+		//Row multiplied by column
+		for(int n = 0; n < N; n++)
+		{
+			sum += input_matrix[i][n] * input_matrix[n][entry -> j];
+		}
+		//assign the sum to its coordinate
+		threaded[i][entry -> j] = sum;
 	}
-	//assign the sum to its coordinate
-	threaded[entry -> i][entry -> j] = sum;
 
 	//Exit the thread
 	pthread_exit(0);
@@ -51,11 +64,6 @@ static inline void *squared_thread_method(void *param)
 
 	//Exit the thread
 	pthread_exit(0);
-}
-
-unsigned long get_random()
-{
-	return rand() % 100;
 }
 
 int main()
@@ -93,31 +101,47 @@ int main()
 	elapsed = end.tv_sec - begin.tv_sec;
 	elapsed += (end.tv_nsec - begin.tv_nsec) / 1000000000.0;
 
-	printf("%s%f%s\n", "threadless completed in ", elapsed, " seconds");
+	printf("%s%f%s\n", "threadless completed in ", elapsed, " seconds");	
+	//print result
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			printf("%llu ", threadless[i][j]);
+		}
+		printf("\n");
+	}
+
 
 	//multiply matrices using N threads
 	printf("%s\n", "threaded starting");
 	clock_gettime(CLOCK_MONOTONIC, &begin);
-	for(i = 0; i < N; i++)
+	for(j = 0; j < N; j++)
 	{
-   		for(j = 0; j < N; j++)
-   		{
-	   		//Assign a row and column for each thread
-			struct index *entry = (struct index*) malloc(sizeof(struct index));
-			entry -> i = i;
-			entry -> j = j;
-			/* Now create the thread passing it data as a parameter */
-			pthread_t thread_id;       //Thread ID
-			//Create the thread
-			pthread_create(&thread_id, NULL, threaded_method, entry);
-			//Make sure the parent waits for all thread to complete
-			pthread_join(thread_id, NULL);
-		}
+   		//Assign a row and column for each thread
+		struct row *entry = (struct row*) malloc(sizeof(struct row));
+		entry -> j = j;
+		//Now create the thread passing the entry as a parameter
+		pthread_t thread_id;       //Thread ID
+		//Create the thread
+		pthread_create(&thread_id, NULL, threaded_method, entry);
+		//Make sure the parent waits for all thread to complete
+		pthread_join(thread_id, NULL);
 	}
 	clock_gettime(CLOCK_MONOTONIC, &end);
 	elapsed = end.tv_sec - begin.tv_sec;
 	elapsed += (end.tv_nsec - begin.tv_nsec) / 1000000000.0;
 	printf("%s%f%s\n", "threaded completed in ", elapsed, " seconds");
+	//print result
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			printf("%llu ", threaded[i][j]);
+		}
+		printf("\n");
+	}
+
 
 	//multiply matrices using N^2 threads
 	printf("%s\n", "squared_threaded starting");
@@ -130,7 +154,7 @@ int main()
 			struct index *entry = (struct index*) malloc(sizeof(struct index));
 			entry -> i = i;
 			entry -> j = j;
-			/* Now create the thread passing it data as a parameter */
+			//Now create the thread passing the entry as a parameter
 			pthread_t thread_id;       //Thread ID
 			//Create the thread
 			pthread_create(&thread_id, NULL, squared_thread_method, entry);
@@ -142,17 +166,15 @@ int main()
 	elapsed = end.tv_sec - begin.tv_sec;
 	elapsed += (end.tv_nsec - begin.tv_nsec) / 1000000000.0;
 	printf("%s%f%s\n", "squared_threaded completed in ", elapsed, " seconds");
-
-/*
 	//print result
 	for (i = 0; i < N; i++)
 	{
 		for (j = 0; j < N; j++)
 		{
-			printf("%llu ", c[i][j]);
+			printf("%llu ", squared_threads[i][j]);
 		}
 		printf("\n");
 	}
-*/
+
 	return 0;
 }
